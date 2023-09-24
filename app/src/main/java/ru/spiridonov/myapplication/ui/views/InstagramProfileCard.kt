88dd1e8a0f9db1944b81e3a0006dded75c48dd63
leonidSpiri <2003.leonid2003@gmail.com>
2023-09-25 00:07:25 +0300

    cool shit

--- app/src/main/java/ru/spiridonov/myapplication/ui/views/InstagramProfileCard.kt
+++ app/src/main/java/ru/spiridonov/myapplication/ui/views/InstagramProfileCard.kt
@@ -1,16 +1,18 @@
 package ru.spiridonov.myapplication.ui.views
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -18,12 +20,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import ru.spiridonov.myapplication.R
 import ru.spiridonov.myapplication.ui.theme.ApplicationTheme
 
 
@@ -39,23 +44,47 @@ fun InstagramProfileCard() {
         colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.background),
         border = BorderStroke(1.dp, MaterialTheme.colorScheme.onBackground)
     ) {
-
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            verticalAlignment = Alignment.CenterVertically
+        Column(
+            modifier = Modifier.padding(16.dp)
         ) {
-            Box(
+
+            Row(
                 modifier = Modifier
-                    .size(50.dp)
-                    .background(color = Color.Yellow)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    modifier = Modifier
+                        .size(60.dp)
+                        .clip(CircleShape)
+                        .background(Color.White)
+                        .padding(8.dp),
+                    painter = painterResource(id = R.drawable.ic_instagram),
+                    contentDescription = ""
+                )
+                UserStatistics(title = "Posts", value = "1950")
+                UserStatistics(title = "Followers", value = "234M")
+                UserStatistics(title = "Following", value = "23")
+            }
+            Text(
+                text = "Instagram",
+                fontSize = 32.sp,
+                fontFamily = FontFamily.Cursive
             )
-            UserStatistics(title = "Posts", value = "1950")
-            UserStatistics(title = "Followers", value = "234M")
-            UserStatistics(title = "Following", value = "23")
+            Text(
+                text = "#Developer",
+                fontSize = 14.sp
+            )
+            Text(
+                text = "notVK.com",
+                fontSize = 14.sp
+            )
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Follow")
+            }
         }
+
     }
 }
 
@@ -85,7 +114,7 @@ private fun UserStatistics(
 
 @Preview
 @Composable
-fun previewCardLight() {
+fun PreviewCardLight() {
     ApplicationTheme(
         darkTheme = false
     ) {
@@ -95,7 +124,7 @@ fun previewCardLight() {
 
 @Preview
 @Composable
-fun previewCardDark() {
+fun PreviewCardDark() {
     ApplicationTheme(
         darkTheme = true
     ) {
